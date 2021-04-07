89471851bd525013fe84d58c2b68d7c8c5b833a9
Shunsuke Kamada <mod.rab7219@gmail.com>
2021-04-07 11:19:45 +0900

    initialize projects

---
+++ swiftui-playgrounds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-playgrounds
+//
+//  Created by 鎌田峻輔 on 2021/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-playgrounds/swiftui_playgroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_playgroundsApp.swift
+//  swiftui-playgrounds
+//
+//  Created by 鎌田峻輔 on 2021/04/07.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_playgroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
